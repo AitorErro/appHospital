c944ea56d6325f05e5d956ac64a67236fe4942cd
AitorErro <aitor@cysnet.es>
2017-07-28 12:27:28 +0200

    Cambios hasta definir PersonDBInitializer() en el Application_Start() del Global.asax

---
+++ AppHospital/App_Start/PersonaDBInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using AppHospital.DataAccess;
+using AppHospital.Models;
+
+namespace AppHospital.App_Start
+{
+    public class PersonaDBInitializer : DropCreateDatabaseAlways<DataAccess.PersonaDBContext>
+    {
+        protected override void Seed(DataAccess.PersonaDBContext context)
+
+        {
+
+            var personas = new List<Persona>
+            {
+                new Persona { nombre = "Julio", apellidos = "Avellaneda Perez"},
+                new Persona { nombre = "Juan", apellidos = "Ruiz del Río"},
+                new Persona { nombre = "Roberto", apellidos = "Alvarado Ramiro"},
+                new Persona { nombre = "Nicolas", apellidos = "Herrera Hervas"},
+                new Persona { nombre = "Jorge", apellidos = "Ramirez Iriarte"},
+                new Persona { nombre = "Nelson", apellidos = "Venegas Ortiz"}
+            };
+
+            personas.ForEach(c => context.Persona.Add(c));
+            context.SaveChanges();
+
+        }
+
+    }
+}
--- AppHospital/Global.asax.cs
+++ AppHospital/Global.asax.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AppHospital.App_Start;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -19,7 +16,7 @@ namespace AppHospital
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            Database.SetInitializer(new );  
+            Database.SetInitializer(new PersonaDBInitializer());  
         }
     }
 }
